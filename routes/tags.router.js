c5bbf6bade1b2286226ae49f3eac55a4c4b57849
Suman Bandita <sumanbandita@gmail.com>
2018-04-26 12:14:57 -0400

    get by id

--- routes/tags.router.js
+++ routes/tags.router.js
@@ -4,12 +4,27 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-/* ========= POST =======*/
+/* ========= GET =======*/
 router.get('/tags',(req,res,next) => {
   knex
     .select('id','name')
     .from('tags')
-    .then(results => {res.json(results)})
+    .then(results => {
+      res.json(results)
+    })
+    .catch(err => next(err));
+});
+
+/* ========= GET BY ID=======*/
+router.get('/tags/:id',(req,res,next) => {
+  const id = req.params.id;
+  knex
+    .select('id','name')
+    .from('tags')
+    .where({id : id})    
+    .then(results => {
+      res.json(results[0]);
+    })
     .catch(err => next(err));
 });
 
